cd8170903a78e4346299a7be60a87bff0acffe00
caioflima <caioflima98@gmail.com>
2023-01-17 18:26:23 -0300

    Subindo projeto 2U laravel

---
+++ caio_lima/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h1>Caio otário</h1>
+    </body>
+</html>
